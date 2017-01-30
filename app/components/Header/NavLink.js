a856fc1216c08f9c2e69ba3748b291127ece1fb8
Paulin Trognon <paulin.trognon@gmail.com>
2017-01-30 13:32:33 +0100

    style navbar links

--- app/components/Header/NavLink.js
+++ app/components/Header/NavLink.js
@@ -2,10 +2,9 @@ import { Link } from 'react-router';
 import styled from 'styled-components';
 
 export default styled(Link)`
-  color: #41ADDD;
-
-  &:active {
-    background: #41ADDD;
-    color: #FFF;
-  }
+  color: #555555;
+  text-decoration: none;
+  font-size: 1.2em;
+  padding-bottom: 2px;
+  border-bottom: solid 3px #000000;
 `;
--- app/components/Header/Wrapper.js
+++ app/components/Header/Wrapper.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 const Wrapper = styled.header`
   text-align: center;
+  box-shadow: 0px 0px 2px rgba(0, 0, 0, 0.5);
 `;
 
 export default Wrapper;

d14ff31b1ed394e30df5b2830ea3f38fcd33310b
Paulin Trognon <paulin.trognon@gmail.com>
2017-01-30 12:30:16 +0100

    correctly center menu

--- app/components/Header/Li.js
+++ app/components/Header/Li.js
@@ -1,9 +1,6 @@
 import styled from 'styled-components';
 
-const padding = '5%';
-
 export default styled.li`
   display: inline-block;
-  padding-left: ${padding};
-  padding-right: ${padding};
+  width: 20%;
 `;

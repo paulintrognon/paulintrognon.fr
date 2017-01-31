510541e10231e8c6bd509d9193c5af3c7ac85e4c
Paulin Trognon <paulin.trognon@gmail.com>
2017-01-31 18:47:42 +0100

    use global-styles insteal of style tag in index.html

--- app/app.js
+++ app/app.js
@@ -27,6 +27,7 @@ import { translationMessages } from './i18n';
 
 // Import the CSS reset, which HtmlWebpackPlugin transfers to the build folder
 import 'sanitize.css/sanitize.css';
+import './global-styles';
 
 // Create redux store with history
 // this uses the singleton browserHistory provided by react-router
--- app/global-styles.js
+++ app/global-styles.js
@@ -8,23 +8,22 @@ injectGlobal`
     width: 100%;
   }
 
-  body {
-    font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
+  * {
+    margin:0;
+    padding:0;
   }
 
-  body.fontLoaded {
-    font-family: 'Open Sans', 'Helvetica Neue', Helvetica, Arial, sans-serif;
+  body {
+    font-family: GidoleRegular, Helvetica, Arial, sans-serif;
   }
 
   #app {
-    background-color: #fafafa;
     min-height: 100%;
     min-width: 100%;
   }
 
   p,
   label {
-    font-family: Georgia, Times, 'Times New Roman', serif;
     line-height: 1.5em;
   }
 `;

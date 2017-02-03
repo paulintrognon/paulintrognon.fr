23aa9419f1df394aa6550ad2a1e3407f4f02307d
Paulin Trognon <paulin.trognon@gmail.com>
2017-02-03 15:30:40 +0100

    adaptative design for section component

--- app/components/Section/Img.js
+++ app/components/Section/Img.js
@@ -9,6 +9,14 @@ const Img = styled(NormalImg)`
   margin-right: 30px;
   margin-bottom: 10px;
   width: 80px;
+
+  @media (max-width: 600px) {
+    width: 50px;
+    margin-top: 0px;
+    margin-left: 0px;
+    margin-right: 10px;
+    margin-bottom: 0px;
+  }
 `;
 
 export default Img;
--- app/components/Section/Text.js
+++ app/components/Section/Text.js
@@ -1,5 +1,7 @@
 import styled from 'styled-components';
 
 export default styled.div`
+@media (min-width: 600px) {
   margin-left: 130px;
+}
 `;

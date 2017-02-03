569a40416ea6f5a553b39a3c2e0a0e67e104b733
Paulin Trognon <paulin.trognon@gmail.com>
2017-02-03 14:38:01 +0100

    use Section components in Skills page

--- app/containers/SkillsPage/JavascriptSkills/index.js
+++ app/containers/SkillsPage/JavascriptSkills/index.js
@@ -2,8 +2,7 @@ import React from 'react';
 import H1 from 'components/H1';
 import H2 from 'components/H2';
 import Article from 'components/Article';
-import Img from 'components/Img';
-import Paragraph from 'components/Paragraph';
+import Section from 'components/Section';
 
 import NodeJsLogo from './nodejs.svg';
 import ReactJsLogo from './reactjs.svg';
@@ -13,29 +12,23 @@ export default () => (
     <H1>
       Javascript Enthusiast
     </H1>
-    <Paragraph>
-      <Img alt="nodejs logo" src={NodeJsLogo} />
-      <div>
-        <H2>NodeJS</H2>
-        <p>
-          5 years experience developping node apps.
-        </p>
-        <p>
-          I am familiar with the micro services paradigm, and have been using NodeJS (with Restify, or Express) for that purpose.
-        </p>
-      </div>
-    </Paragraph>
-    <Paragraph>
-      <Img alt="reactjs logo" src={ReactJsLogo} />
-      <div>
-        <H2>ReactJS</H2>
-        <p>
-          Still learning ReactJS but really like the feeling.
-        </p>
-        <p>
-          This very website has been developped using the React Boilerplate. Checkout the code: github.com/paulintrognon/paulintrognon.fr
-        </p>
-      </div>
-    </Paragraph>
+    <Section img={NodeJsLogo} alt="nodejs logo">
+      <H2>NodeJS</H2>
+      <p>
+        5 years experience developping node apps.
+      </p>
+      <p>
+        I am familiar with the micro services paradigm, and have been using NodeJS (with Restify, or Express) for that purpose.
+      </p>
+    </Section>
+    <Section img={ReactJsLogo} alt="reactjs logo">
+      <H2>ReactJS</H2>
+      <p>
+        Still learning ReactJS but really like the feeling.
+      </p>
+      <p>
+        This very website has been developped using the React Boilerplate. Checkout the code: github.com/paulintrognon/paulintrognon.fr
+      </p>
+    </Section>
   </Article>
 );
--- app/containers/SkillsPage/SearchUxSkills/index.js
+++ app/containers/SkillsPage/SearchUxSkills/index.js
@@ -2,8 +2,7 @@ import React from 'react';
 import H1 from 'components/H1';
 import H2 from 'components/H2';
 import Article from 'components/Article';
-import Img from 'components/Img';
-import Paragraph from 'components/Paragraph';
+import Section from 'components/Section';
 
 import ElasticsearchLogo from './elasticsearch.svg';
 
@@ -12,22 +11,19 @@ export default () => (
     <H1>
       Search UX Guru
     </H1>
-    <Paragraph>
-      <Img alt="elasticsearch logo" src={ElasticsearchLogo} />
-      <div>
-        <H2>ElasticSearch</H2>
-        <p>
-          I have used ElasticSearch for the last 3 years, and am very familiar with its request synthax, as well as its Javascript API.
-        </p>
-        <H2>Designing Search</H2>
-        <p>
-          I have followed those following training courses (by Search Solutions):
-          <ul>
-            <li>Designing Search, by Tony Russell-Rose</li>
-            <li>Evaluating Search, by Paul Clough</li>
-          </ul>
-        </p>
-      </div>
-    </Paragraph>
+    <Section img={ElasticsearchLogo} alt="elasticsearch logo">
+      <H2>ElasticSearch</H2>
+      <p>
+        I have used ElasticSearch for the last 3 years, and am very familiar with its request synthax, as well as its Javascript API.
+      </p>
+      <H2>Designing Search</H2>
+      <p>
+        I have followed those following training courses (by Search Solutions):
+        <ul>
+          <li>Designing Search, by Tony Russell-Rose</li>
+          <li>Evaluating Search, by Paul Clough</li>
+        </ul>
+      </p>
+    </Section>
   </Article>
 );

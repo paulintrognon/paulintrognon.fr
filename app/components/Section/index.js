6618e93f63c35f35480c678666fc68db4bd931b8
Paulin Trognon <paulin.trognon@gmail.com>
2017-02-03 14:47:44 +0100

    forgot to wrap text with Text component in Section

--- app/components/Section/index.js
+++ app/components/Section/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 
 import Img from './Img';
+import Text from './Text';
 import Wrapper from './Wrapper';
 
 function List(props) {
   return (
     <Wrapper>
       {props.img ? <Img src={props.img} alt={props.alt} /> : ''}
-      {props.children}
+      <Text>
+        {props.children}
+      </Text>
     </Wrapper>
   );
 }
--- app/components/Section/tests/index.test.js
+++ app/components/Section/tests/index.test.js
@@ -3,11 +3,13 @@ import { shallow } from 'enzyme';
 import React from 'react';
 
 import Img from '../Img';
+import Text from '../Text';
 import Section from '../index';
 import Wrapper from '../Wrapper';
 
 describe('<Section />', () => {
   it('should render a Wrapper', wrapperTest);
+  it('should put children in Text', renderTextTest);
   it('should render an img if given', renderImgTest);
 });
 
@@ -15,6 +17,12 @@ function wrapperTest() {
   const renderedComponent = shallow(<Section />);
   expect(renderedComponent.find(Wrapper).length).toEqual(1);
   expect(renderedComponent.find(Img).length).toEqual(0);
+}
+
+function renderTextTest() {
+  const children = 'children';
+  const renderedComponent = shallow(<Section>{children}</Section>);
+  expect(renderedComponent.contains(<Text>{children}</Text>)).toEqual(true);
 }
 
 function renderImgTest() {

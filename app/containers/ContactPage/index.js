157adf7ddfb585297f07d7cf604a6d2fe094170a
Paulin Trognon <paulin.trognon@gmail.com>
2017-02-08 17:54:43 +0100

    use IconLink in MediaIcon

--- app/containers/ContactPage/index.js
+++ app/containers/ContactPage/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Helmet from 'react-helmet';
 
+import A from 'components/A';
 import Article from 'components/Article';
+import IconLink from 'components/IconLink';
 import Wrapper from './Wrapper';
 import Img from './Img';
-import MediaIcon from './MediaIcon';
 
 import PaulinTrognonPortrait from './paulintrognon.jpg';
 import GitHubLogo from './github.svg';
@@ -28,13 +29,11 @@ export default () => (
           <Img src={PaulinTrognonPortrait} alt="Paulin Trognon Portrait" />
         </p>
         <p>
-          Email me at <a href={`mailto:${email}`}>{email}</a>
+          Email me at <A href={`mailto:${email}`}>{email}</A>
         </p>
-        <p>
-          <MediaIcon src={GitHubLogo} alt="GitHub Logo" />
-          <MediaIcon src={LinkedinLogo} alt="GitHub Logo" />
-          <MediaIcon src={StackOverflowLogo} alt="GitHub Logo" />
-        </p>
+        <IconLink img={GitHubLogo} alt="GitHub Logo" link="a">GitHub</IconLink>
+        <IconLink img={LinkedinLogo} alt="GitHub Logo" link="b">LinkedIn</IconLink>
+        <IconLink img={StackOverflowLogo} alt="GitHub Logo" link="c">Stack Overflow</IconLink>
       </Wrapper>
     </Article>
   </div>

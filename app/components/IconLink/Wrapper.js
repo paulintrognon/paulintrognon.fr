3b772ab9d799dc9264fc5a5c6934413c179c1ce4
Paulin Trognon <paulin.trognon@gmail.com>
2017-02-16 12:40:34 +0100

    restyle of contact page icon links

--- app/components/IconLink/Wrapper.js
+++ app/components/IconLink/Wrapper.js
@@ -3,11 +3,17 @@ import styled from 'styled-components';
 import AOut from 'components/AOut';
 
 const Wrapper = styled(AOut)`
+  text-decoration: none;
   display: inline-block;
   width: 150px;
   margin-right: 25px;
   margin-left: 25px;
-  margin-top: 50px;
+  margin-top: 100px;
+  background-color: white;
+  transition: background-color .2s ease-out;
+  padding-top: 15px;
+  padding-bottom: 10px;
+  border-radius: 10px;
 
   img {
     width: 50px;
@@ -18,8 +24,12 @@ const Wrapper = styled(AOut)`
     transition: color .2s ease-out;
   }
 
+  &:hover {
+    background-color: #fefe00;
+  }
+
   &:hover .icon-link-desc {
-    color: #555555;
+    color: black;
   }
 `;
 

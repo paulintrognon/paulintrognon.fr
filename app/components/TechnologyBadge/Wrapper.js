a9792cebf87b86028dbdb96501badd13f981cb1c
Paulin Trognon <paulin.trognon@gmail.com>
2017-04-06 15:41:01 +0200

    add TechnologyBadges

---
+++ app/components/TechnologyBadge/Wrapper.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.p`
+  display: inline-block;
+  margin-right: 20px;
+  margin-bottom: 5px;
+
+  img {
+    width: 30px;
+    margin-right: 10px;
+  }
+`;
+
+export default Wrapper;

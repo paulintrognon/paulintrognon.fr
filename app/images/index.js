b6be3f6e19e355dc717a06881edacca6f4236c24
Paulin Trognon <paulin.trognon@gmail.com>
2017-04-06 15:40:43 +0200

    add images

---
+++ app/images/index.js
@@ -0,0 +1,47 @@
+import React from 'react';
+
+import AngularImg from './angular.svg';
+import DynamoDbImg from './dynamodb.svg';
+import DebianImg from './debian.svg';
+import ElasticSearchImg from './elasticsearch.svg';
+import Html5Img from './html5.svg';
+import JQueryImg from './jquery.svg';
+import NodeJsImg from './nodejs.svg';
+import PhpImg from './php.svg';
+import RedHatImg from './redhat.svg';
+
+export function AngularLogo() {
+  return <img alt="angular logo" src={AngularImg} />;
+}
+
+export function DebianLogo() {
+  return <img alt="debian logo" src={DebianImg} />;
+}
+
+export function DynamoDbLogo() {
+  return <img alt="dynamodb logo" src={DynamoDbImg} />;
+}
+
+export function ElasticSearchLogo() {
+  return <img alt="elasticsearch logo" src={ElasticSearchImg} />;
+}
+
+export function Html5Logo() {
+  return <img alt="HTML5 logo" src={Html5Img} />;
+}
+
+export function JQueryLogo() {
+  return <img alt="jQuery logo" src={JQueryImg} />;
+}
+
+export function NodeJsLogo() {
+  return <img alt="nodejs logo" src={NodeJsImg} />;
+}
+
+export function PhpLogo() {
+  return <img alt="php logo" src={PhpImg} />;
+}
+
+export function RedHatLogo() {
+  return <img alt="RedHat logo" src={RedHatImg} />;
+}

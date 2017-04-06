a916c142d5a15a70dbb5734cd3962cd46fd4a16b
Paulin Trognon <paulin.trognon@gmail.com>
2017-04-06 15:41:22 +0200

    add experience component

--- app/components/Article/index.js
+++ app/components/Article/index.js
@@ -1,10 +1,5 @@
 import styled from 'styled-components';
 
 export default styled.article`
-  @media (min-width: 1200px) {
-    display: inline-block;
-    vertical-align: top;
-    width: 46%;
-    margin: 0px 2% 50px 2%;
-  }
+  margin: 0 10%;
 `;
---
+++ app/components/Experience/index.js
@@ -0,0 +1,35 @@
+import styled from 'styled-components';
+
+export default styled.div`
+  padding: 15px;
+  margin-bottom: 40px;
+
+  h1 {
+    margin-top: -5px;
+    margin-left: 415px;
+  }
+
+  h2 {
+    margin-bottom: 3px;
+  }
+
+  img.side-image {
+    float: left;
+    border: 1px solid #888;
+    width: 400px;
+  }
+
+  .technology-container {
+    margin-left: 415px;
+  }
+
+  ul {
+    margin-top: 10px;
+    margin-left: 422px;
+  }
+
+  ul li {
+    list-style-type: '-';
+    padding-left: 5px;
+  }
+`;

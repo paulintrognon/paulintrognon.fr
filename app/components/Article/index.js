fa3102ba20b31739c4fe3d56480a0510951de89f
Paulin Trognon <paulin.trognon@gmail.com>
2017-04-10 11:00:43 +0200

    some style tweaks

--- app/components/Article/index.js
+++ app/components/Article/index.js
@@ -2,4 +2,5 @@ import styled from 'styled-components';
 
 export default styled.article`
   margin: 0 10%;
+  margin-bottom: 20px;
 `;
--- app/components/Section/Img.js
+++ app/components/Section/Img.js
@@ -4,7 +4,7 @@ import NormalImg from 'components/Img';
 
 const Img = styled(NormalImg)`
   float: left;
-  margin-top: 10px;
+  margin-top: 0px;
   margin-left: 0px;
   margin-right: 30px;
   margin-bottom: 10px;
@@ -12,7 +12,6 @@ const Img = styled(NormalImg)`
 
   @media (max-width: 600px) {
     width: 50px;
-    margin-top: 0px;
     margin-left: 0px;
     margin-right: 10px;
     margin-bottom: 0px;

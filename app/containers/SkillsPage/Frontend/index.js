283de8165689b237806564523bd9483fa0959920
Paulin Trognon <paulin.trognon@gmail.com>
2017-12-06 16:03:50 +0100

    update frontend section

--- app/containers/SkillsPage/Frontend/index.js
+++ app/containers/SkillsPage/Frontend/index.js
@@ -8,40 +8,37 @@ import SectionHead from 'components/SectionHead';
 import ReactJsLogo from './reactjs.svg';
 import AngularLogo from './angular.svg';
 import JQueryLogo from './jquery.svg';
-import Css3Logo from './css.svg';
 
 export default () => (
   <Article>
     <SectionHead title="Développeur Frontend" />
     <Section img={AngularLogo} alt="angular logo">
-      <h3>Angular 1</h3>
+      <h3>Angular 1 à 4</h3>
       <p>
         3 ans d&#8217;éxperience au sein de l&#8217;entreprise <AText href="https://findout.com">Findout</AText>
       </p>
       <p>
         Développement d&#8217;interfaces, avec tests unitaires et tests E2E
       </p>
+      <p>
+        Développement d&#8217;une de type back-office en Angular 4
+      </p>
     </Section>
     <Section img={ReactJsLogo} alt="reactjs logo">
       <h3>ReactJS</h3>
       <p>
-        Ce site a été codé en ReactJS : <AText href="https://github.com/paulintrognon/paulintrognon.fr">github.com/paulintrognon.fr</AText>
+        C&#8217;est avec cette technologie, qui m&#8217;enthousiasme beaucoup, que je réalise mes projets personnels,
+        comme <AText href="http://www.music-genres.com">music-genres</AText>, ou <AText href="https://plouf-plouf.fr">Plouf Plouf</AText>.
       </p>
       <p>
-        Je suis en cours de formation, et cette technologie m&#8217;enthousiasme beaucoup
+        J&#8217;utilise le pattern Redux
       </p>
     </Section>
     <Section img={JQueryLogo} alt="jquery logo">
-      <h3>JQuery</h3>
+      <h3>Technologies traditionnelles</h3>
       <p>
-        Très à l&#8217;aise avec ce framework, pour l&#8217;avoir utilisé dans plusieurs projets pro
+        Je suis très à l&#8217;aise avec jQuery, pour l&#8217;avoir utilisé dans plusieurs projets pro ; Je maitrise de manière générale le JavaScript côté Frontend
       </p>
-      <p>
-        Je maitrise de manière générale le JavaScript côté Frontend
-      </p>
-    </Section>
-    <Section img={Css3Logo} alt="css3 logo">
-      <h3>CSS 3 & responsive</h3>
       <p>
         Bonnes connaissances en CSS3, maitrise des medias queries (possibilité de rendre un site responsive)
       </p>

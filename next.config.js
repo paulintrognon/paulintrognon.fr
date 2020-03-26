b9a41e3d1fa78752d1bd4a03a1eadbdcdb6ec6c4
Paulin Trognon <paulin.trognon@gmail.com>
2020-03-26 17:53:51 +0100

    add md blog test

--- next.config.js
+++ next.config.js
@@ -2,4 +2,12 @@ const withCSS = require('@zeit/next-css');
 
 module.exports = withCSS({
   cssModules: true,
+  webpack: cfg => {
+    cfg.module.rules.push({
+      test: /\.md$/,
+      loader: 'frontmatter-markdown-loader',
+      options: { mode: ['react-component'] },
+    });
+    return cfg;
+  },
 });
---
+++ pages/blog.js
@@ -0,0 +1,25 @@
+import Head from 'next/head';
+import { attributes, react as HomeContent } from '../content/blog.md';
+
+export default () => {
+  const { title, cats } = attributes;
+  return (
+    <>
+      <Head>
+        <script src="https://identity.netlify.com/v1/netlify-identity-widget.js" />
+      </Head>
+      <article>
+        <h1>{title}</h1>
+        <HomeContent />
+        <ul>
+          {cats.map((cat, k) => (
+            <li key={k}>
+              <h2>{cat.name}</h2>
+              <p>{cat.description}</p>
+            </li>
+          ))}
+        </ul>
+      </article>
+    </>
+  );
+};

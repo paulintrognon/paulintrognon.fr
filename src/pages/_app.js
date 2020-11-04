f2d589fb9afb1a6862cd46b0c52292dcba7f9ac0
Paulin Trognon <paulin.trognon@gmail.com>
2020-11-04 23:28:01 +0100

    Analytics: add Plausible script

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Router from 'next/router';
+import Head from 'next/head';
 import '../reset.css';
 import '../fonts.css';
 import 'highlight.js/styles/agate.css';
@@ -10,5 +11,17 @@ Router.events.on('routeChangeComplete', () => {
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <script
+          async
+          defer
+          data-domain="paulintrognon.fr"
+          src="https://analytiques.paulintrognon.fr/js/plausible.js"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
